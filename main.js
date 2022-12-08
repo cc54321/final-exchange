c2aac0cf0665164e723dbe615b0ee246a27cf607
dd-code-immersives <domilamad89@gmail.com>
2022-12-08 11:54:02 -0500

    fixed html error, and reformatted event listener

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ let currencyForm = document.querySelector("#currency-form");
 let currencyInput = document.querySelector("#currency-input");
 let exchangeRate = document.querySelector('#exchange-rate');
 
+let description = document.querySelector('#desc');
+let pair = document.querySelector('#pair');
+let exchange = document.querySelector('#exchange');
 
 
 //https://api.api-ninjas.com/v1/exchangerate?pair=
@@ -33,25 +36,23 @@ $.ajax({
     }
 });
 */
-currencyForm.addEventListener('submit', (e) => {
+exchangeRate.addEventListener('submit', (e) => {
     e.preventDefault();
     //console.log(input.value)
+    console.log("submit triggered");
     fetch(`https://api.api-ninjas.com/v1/exchangerate?pair=${currencyInput.value}`, {
-    method  : 'get',
-    headers : new Headers({
-        'Content-Type': 'application/json',
-        'X-Api-Key': 'vba/K2DVf9EJ+xHNbnqvCg==viklDz9EDm221QfL'
-    })
-}).then( response => {
-    return response.json(); 
-}).then(data => {
-            console.log(data)
+    method  : 'GET',
+    headers: {'Content-Type': 'application/json',
+        'X-Api-Key': 'vba/K2DVf9EJ+xHNbnqvCg==viklDz9EDm221QfL' }
+    }).then( response => {
+        return response.json(); 
+    }).then(data => {
+        console.log(data)
            
 
-            //currencies.innerText = input.value
-            currencies.innerHTML = data.currency-pair;
-            exchangeRate.innerHTML = data.exchange-rate;
-            description.innerHTML = data.description;
-
-        });
+        //currencies.innerText = input.value
+        currencies.innerHTML = data.currency_pair;
+        exchangeRate.innerHTML = data.exchange_rate;
+        //description.innerHTML = data.description;
+    });
 });
